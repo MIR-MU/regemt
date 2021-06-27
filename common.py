6ae3ea39aa4599d556283163aca4a04d37d39fdd
stefanik12 <7cb4bc89>
2021-06-27 13:21:34 +0200

    MQM judgements

--- common.py
+++ common.py
@@ -1,7 +1,8 @@
 import abc
 import os
-from typing import List, Tuple, Iterable, Dict
+from typing import List, Tuple, Iterable, Dict, Optional, Set, Any
 import pandas as pd
+from gensim.utils import simple_preprocess
 from tqdm import tqdm
 
 TRAIN_DATASET_FILE_TEMPLATE = "DAseg-wmt-newstest2015/DAseg.newstest2015.%s.%s"
@@ -16,19 +17,41 @@ class Judgements:
         self.translations = translations
         self.scores = scores
 
+    def get_tokenized_texts(self, stopwords: Optional[Set] = None,
+                            desc: Optional[str] = None) -> Iterable[Tuple[List[str], List[str]]]:
+        if not stopwords:
+            stopwords = set()
+        corpus = zip(self.references, self.translations)
+        if desc:
+            corpus = tqdm(corpus, desc=desc, total=len(self))
+        for reference, translation in corpus:
+            reference_words = [w.lower() for w in simple_preprocess(reference[0]) if w.lower() not in stopwords]
+            translation_words = [w.lower() for w in simple_preprocess(translation) if w.lower() not in stopwords]
+            yield reference_words, translation_words
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Judgements):
+            return NotImplemented
+        return all([
+            self.src_texts == other.src_texts,
+            self.references == other.references,
+            self.translations == other.translations,
+            self.scores == other.scores,
+        ])
+
     def __len__(self):
         return len(self.src_texts)
 
 
 class Metric(abc.ABC):
-    label: str = None
+    label: str = 'None'
 
     @abc.abstractmethod
-    def fit(self, judgements: Judgements):
+    def fit(self, train_judgements: Judgements, test_judgements: Judgements):
         pass
 
     @abc.abstractmethod
-    def compute(self, judgements: Judgements) -> List[float]:
+    def compute(self, test_judgements: Judgements) -> List[float]:
         pass
 
 
@@ -40,18 +63,28 @@ class Evaluator:
     langs = ["cs-en", "de-en", "fi-en", "ru-en"]
     langs_psqm = ["zh-en"]
 
-    def __init__(self, data_dir: str, lang_pair: Tuple[str, str], metrics: List[Metric], psqm: bool = False):
+    def __init__(self, data_dir: str, lang_pair: Tuple[str, str], metrics: List[Metric], judgements_type: str = "DA"):
         self.lang_pair = lang_pair
         self.data_dir = data_dir
         self.metrics = metrics
-        self.psqm = psqm
+        self.judgements_type = judgements_type
 
         train_judgements = self.load_judgements("train")
+        test_judgements = self.load_judgements("test")
         for metric in self.metrics:
-            metric.fit(train_judgements)
+            metric.fit(train_judgements, test_judgements)
 
-    def load_judgements(self, split: str = "train", firstn: int = 10000) -> Judgements:
-        if self.psqm:
+    def load_judgements(self, split: str = "train", firstn: int = 10000, error_type: str = None) -> Judgements:
+        if self.judgements_type == "DA":
+            # TODO: note that train and test datasets are the same now
+            split_file_template = os.path.join(self.data_dir, TEST_DATASET_FILE_TEMPLATE if split == "train"
+                                               else TEST_DATASET_FILE_TEMPLATE)
+            src_texts = self._load_file(split_file_template % ("source", self.lang_pair))
+            references = [[ref] for ref in self._load_file(split_file_template % ("reference", self.lang_pair))]
+            translations = self._load_file(split_file_template % ("mt-system", self.lang_pair))
+            scores = [float(s) for s in self._load_file(split_file_template % ("human", self.lang_pair))]
+
+        elif self.judgements_type == "PSQM":
             split_file_template = os.path.join(self.data_dir, "psqm_newstest2020_zhen.tsv")
             all_df = pd.read_csv(split_file_template, sep="\t")
             all_df = all_df.set_index(["doc_id", "seg_id"])
@@ -75,24 +108,59 @@ class Evaluator:
                         references.append([max_scored_reference.target])
                         translations.append(judgement.target)
                         scores.append(judgement.score)
-                    break  # we want variable translation pairs, but if more is needed, we can remove this
+                        break  # we want variable translation pairs, but if more is needed, we can remove this
                 else:
                     print("No reference judgements: %s" % i)
                 if len(src_texts) >= firstn:
                     break
-        else:
-            split_file_template = os.path.join(self.data_dir, TRAIN_DATASET_FILE_TEMPLATE if split == "train"
-                                                              else TEST_DATASET_FILE_TEMPLATE)
+
+        elif self.judgements_type == "DA":
+            # TODO: note that train and test datasets are the same now
+            split_file_template = os.path.join(self.data_dir, TEST_DATASET_FILE_TEMPLATE if split == "train"
+                                               else TEST_DATASET_FILE_TEMPLATE)
             src_texts = self._load_file(split_file_template % ("source", self.lang_pair))
             references = [[ref] for ref in self._load_file(split_file_template % ("reference", self.lang_pair))]
             translations = self._load_file(split_file_template % ("mt-system", self.lang_pair))
             scores = [float(s) for s in self._load_file(split_file_template % ("human", self.lang_pair))]
+        elif self.judgements_type == "MQM":
+            df = pd.read_csv(os.path.join(self.data_dir, "mqm_newstest2020_zhen.tsv"), sep="\t")
+            df = df.set_index(["system", "seg_id"])
+
+            judgements_df = pd.read_csv(os.path.join(self.data_dir, "mqm_newstest2020_zhen.avg_seg_scores.tsv"), sep=" ")
+            judgements_df = judgements_df.set_index(["system", "seg_id"])
+
+            df = df.join(judgements_df)
+            df = df.reset_index().set_index(["seg_id", "doc_id", "rater"])
+            system_df = df[~df["system"].isin(["Human-A.0", "Human-B.0"])]
+
+            human_df = df[df["system"].isin(["Human-A.0", "Human-B.0"])]
+            human_df_ok = human_df[human_df.category == "no-error"]
+            human_df_translated = human_df_ok.join(system_df, lsuffix="_human", rsuffix="_system")
+
+            all_references = human_df_translated["target_human"].groupby(level=[0, 1, 2]).unique()
+            all_references.name = 'all_references'
+            human_df_translated_ref = human_df_translated.join(all_references)
+
+            translated_clean_df = human_df_translated_ref[~pd.isna(human_df_translated_ref).any(axis=1)]
+
+            if error_type is None:
+                selected_df = translated_clean_df
+            else:
+                selected_df = translated_clean_df[translated_clean_df["category_system"] == error_type]
+
+            return Judgements(selected_df["source_system"].values,
+                              selected_df["all_references"].values,
+                              selected_df["target_system"].values,
+                              selected_df["mqm_avg_score_system"].values)
+        else:
+            raise ValueError(self.judgements_type)
 
         return Judgements(src_texts, references, translations, scores)
 
-    def _load_file(self, fpath: str) -> List[str]:
+    @staticmethod
+    def _load_file(fpath: str) -> List[str]:
         with open(fpath) as f:
-            return [l.strip() for l in f.readlines()]
+            return [line.strip() for line in f.readlines()]
 
     def evaluate(self) -> Dict[str, List[float]]:
         report = {}
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+from typing import Optional
+
 from scipy.stats import spearmanr
 
 from bertscore import BERTScore
 from conventional_metrics import BLEU, METEOR
-from scm import SCM
+from scm import SCM, ContextualSCM
 from wmd import WMD
 from common import Evaluator
 import pandas as pd
 
 if __name__ == '__main__':
-    USE_PSQM_JUDGEMENTS = True
+    JUDGEMENTS_TYPE = "MQM"
 
     metrics = [
         BLEU(),
         METEOR(),
         # BERTScore(tgt_lang="en"),
+        ContextualSCM(tgt_lang="en"),
+        # SCM(tgt_lang="en", use_tfidf=False),
         # SCM(tgt_lang="en", use_tfidf=True),
         # SCM(tgt_lang="en", use_tfidf=False),
         # WMD(tgt_lang="en"),
@@ -21,11 +25,11 @@ if __name__ == '__main__':
     correlations = {m.label: {} for m in metrics}
     correlations["human"] = {}
 
-    langs = Evaluator.langs_psqm if USE_PSQM_JUDGEMENTS else Evaluator.langs
+    langs = Evaluator.langs_psqm if "QM" in JUDGEMENTS_TYPE is not None else Evaluator.langs
 
     for lang_pair in [pair for pair in langs if pair.split("-")[-1] == "en"]:
         print("Evaluating lang pair %s" % lang_pair)
-        evaluator = Evaluator("data_dir", lang_pair, metrics, psqm=True)
+        evaluator = Evaluator("data_dir", lang_pair, metrics, judgements_type=JUDGEMENTS_TYPE)
         report = evaluator.evaluate()
         print(report)
         human_judgements = report["human"]
