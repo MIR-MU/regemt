2bb175c182c6f427b74ba5ff2e07a5f39c571151
Vít Novotný <witiko@mail.muni.cz>
2021-07-09 16:10:06 +0200

    Evaluate WMT in CI

--- main.py
+++ main.py
@@ -10,50 +10,50 @@ from scm import SCM, ContextualSCM, DecontextualizedSCM  # noqa: F401
 from wmd import WMD, ContextualWMD, DecontextualizedWMD  # noqa: F401
 
 if __name__ == '__main__':
-    JUDGEMENTS_TYPE = "MQM"
-    reports = []
+    for judgements_type in ['DA', 'MQM']:
+        reports = []
 
-    langs = Evaluator.langs_for_judgements(JUDGEMENTS_TYPE)
+        langs = Evaluator.langs_for_judgements(judgements_type)
 
-    for lang_pair in langs:
-        tgt_lang = lang_pair.split("-")[-1]
-        metrics = [
-            BLEU(),
-            METEOR(),
-            BERTScore(tgt_lang=tgt_lang),
-            ContextualSCM(tgt_lang=tgt_lang),
-            ContextualWMD(tgt_lang=tgt_lang),
-            DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=False),
-            DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=True),
-            DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=False),
-            DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=True),
-            # SCM(tgt_lang="en", use_tfidf=False),
-            # SCM(tgt_lang="en", use_tfidf=True),
-            # WMD(tgt_lang="en", use_tfidf=False),
-            # WMD(tgt_lang="en", use_tfidf=True),
-            # SyntacticCompositionality needs PoS tagger, that can be automatically resolved only for de + zh
-            # SyntacticCompositionality(tgt_lang=tgt_lang, src_lang="en", reference_free=NO_REFERENCE)
-        ]
+        for lang_pair in langs:
+            tgt_lang = lang_pair.split("-")[-1]
+            metrics = [
+                BLEU(),
+                METEOR(),
+                BERTScore(tgt_lang=tgt_lang),
+                ContextualSCM(tgt_lang=tgt_lang),
+                ContextualWMD(tgt_lang=tgt_lang),
+                DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=False),
+                DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=True),
+                DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=False),
+                DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=True),
+                # SCM(tgt_lang="en", use_tfidf=False),
+                # SCM(tgt_lang="en", use_tfidf=True),
+                # WMD(tgt_lang="en", use_tfidf=False),
+                # WMD(tgt_lang="en", use_tfidf=True),
+                # SyntacticCompositionality needs PoS tagger, that can be automatically resolved only for de + zh
+                # SyntacticCompositionality(tgt_lang=tgt_lang, src_lang="en", reference_free=NO_REFERENCE)
+            ]
 
-        print("Evaluating lang pair %s" % lang_pair)
-        evaluator = Evaluator("data_dir", lang_pair, metrics, judgements_type=JUDGEMENTS_TYPE, firstn=100)
-        report = evaluator.evaluate()
-        reports.append(report)
+            print("Evaluating lang pair %s" % lang_pair)
+            evaluator = Evaluator("data_dir", lang_pair, metrics, judgements_type=judgements_type, firstn=100)
+            report = evaluator.evaluate()
+            reports.append(report)
 
-        pearson = pd.DataFrame(report).applymap(float).corr(method="pearson").applymap(abs)
-        assert pearson.gt(0.001).all(axis=None), pearson
-        sns.heatmap(pearson, annot=True)
-        plt.tight_layout()
-        plt.show()
-        plt.savefig('heatmap-pearson-%s.png' % lang_pair)
-        plt.clf()
+            pearson = pd.DataFrame(report).applymap(float).corr(method="pearson").applymap(abs)
+            assert pearson.gt(0.001).all(axis=None), pearson
+            sns.heatmap(pearson, annot=True)
+            plt.tight_layout()
+            plt.show()
+            plt.savefig('heatmap-pearson-%s-%s.png' % (judgements_type, lang_pair))
+            plt.clf()
 
-        spearman = pd.DataFrame(report).applymap(float).corr(method="spearman").applymap(abs)
-        assert spearman.gt(0.001).all(axis=None), spearman
-        sns.heatmap(spearman, annot=True)
-        plt.show()
-        plt.tight_layout()
-        plt.savefig('heatmap-spearman-%s.png' % lang_pair)
-        plt.clf()
+            spearman = pd.DataFrame(report).applymap(float).corr(method="spearman").applymap(abs)
+            assert spearman.gt(0.001).all(axis=None), spearman
+            sns.heatmap(spearman, annot=True)
+            plt.show()
+            plt.tight_layout()
+            plt.savefig('heatmap-spearman-%s-%s.png' % (judgements_type, lang_pair))
+            plt.clf()
 
     print("Done")
--- main_no_reference.py
+++ main_no_reference.py
@@ -10,47 +10,47 @@ from scm import SCM, ContextualSCM, DecontextualizedSCM  # noqa: F401
 from wmd import WMD, ContextualWMD, DecontextualizedWMD  # noqa: F401
 
 if __name__ == '__main__':
-    JUDGEMENTS_TYPE = "MQM"
-    NO_REFERENCE = True
+    for judgements_type in ['MQM']:
+        NO_REFERENCE = True
 
-    reports = []
+        reports = []
 
-    langs = Evaluator.langs_for_judgements(JUDGEMENTS_TYPE)
+        langs = Evaluator.langs_for_judgements(judgements_type)
 
-    for lang_pair in langs:
-        src_lang, tgt_lang = lang_pair.split("-")
-        metrics = [
-            ContextualWMD(tgt_lang=tgt_lang, reference_free=NO_REFERENCE),
-            BERTScore(tgt_lang=tgt_lang, reference_free=NO_REFERENCE),
-            # ContextualSCM(tgt_lang=tgt_lang, reference_free=NO_REFERENCE),
-            DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=False, reference_free=NO_REFERENCE),
-            DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=True, reference_free=NO_REFERENCE),
-            DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=False, reference_free=NO_REFERENCE),
-            DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=True, reference_free=NO_REFERENCE),
-            # # SyntacticCompositionality needs PoS tagger, that can be automatically resolved only for de + zh
-            SyntacticCompositionality(tgt_lang=tgt_lang, src_lang=src_lang, reference_free=NO_REFERENCE)
-        ]
+        for lang_pair in langs:
+            src_lang, tgt_lang = lang_pair.split("-")
+            metrics = [
+                ContextualWMD(tgt_lang=tgt_lang, reference_free=NO_REFERENCE),
+                BERTScore(tgt_lang=tgt_lang, reference_free=NO_REFERENCE),
+                # ContextualSCM(tgt_lang=tgt_lang, reference_free=NO_REFERENCE),
+                DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=False, reference_free=NO_REFERENCE),
+                DecontextualizedSCM(tgt_lang=tgt_lang, use_tfidf=True, reference_free=NO_REFERENCE),
+                DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=False, reference_free=NO_REFERENCE),
+                DecontextualizedWMD(tgt_lang=tgt_lang, use_tfidf=True, reference_free=NO_REFERENCE),
+                # # SyntacticCompositionality needs PoS tagger, that can be automatically resolved only for de + zh
+                SyntacticCompositionality(tgt_lang=tgt_lang, src_lang=src_lang, reference_free=NO_REFERENCE)
+            ]
 
-        print("Evaluating lang pair %s" % lang_pair)
-        evaluator = Evaluator("data_dir", lang_pair, metrics,
-                              judgements_type=JUDGEMENTS_TYPE, reference_free=NO_REFERENCE, firstn=100)
-        report = evaluator.evaluate()
-        reports.append(report)
+            print("Evaluating lang pair %s" % lang_pair)
+            evaluator = Evaluator("data_dir", lang_pair, metrics,
+                                  judgements_type=judgements_type, reference_free=NO_REFERENCE, firstn=100)
+            report = evaluator.evaluate()
+            reports.append(report)
 
-        pearson = pd.DataFrame(report).applymap(float).corr(method="pearson").applymap(abs)
-        assert pearson.gt(0.001).all(axis=None), pearson
-        sns.heatmap(pearson, annot=True)
-        plt.tight_layout()
-        plt.show()
-        plt.savefig('heatmap_no_reference-pearson-%s.png' % lang_pair)
-        plt.clf()
+            pearson = pd.DataFrame(report).applymap(float).corr(method="pearson").applymap(abs)
+            assert pearson.gt(0.001).all(axis=None), pearson
+            sns.heatmap(pearson, annot=True)
+            plt.tight_layout()
+            plt.show()
+            plt.savefig('heatmap_no_reference-pearson-%s-%s.png' % (judgements_type, lang_pair))
+            plt.clf()
 
-        spearman = pd.DataFrame(report).applymap(float).corr(method="spearman").applymap(abs)
-        assert spearman.gt(0.001).all(axis=None), spearman
-        sns.heatmap(spearman, annot=True)
-        plt.show()
-        plt.tight_layout()
-        plt.savefig('heatmap_no_reference-spearman-%s.png' % lang_pair)
-        plt.clf()
+            spearman = pd.DataFrame(report).applymap(float).corr(method="spearman").applymap(abs)
+            assert spearman.gt(0.001).all(axis=None), spearman
+            sns.heatmap(spearman, annot=True)
+            plt.show()
+            plt.tight_layout()
+            plt.savefig('heatmap_no_reference-spearman-%s-%s.png' % (judgements_type, lang_pair))
+            plt.clf()
 
     print("Done")
